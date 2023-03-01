17e5aaf8871c414d42a0c0e4e38b2bb0ae6ce79b
Anton Naidunou <Anton_Naidunou@epam.com>
2023-03-01 17:59:23 +0100

    Fix that favorites are not loading after page refresh

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+import { MockStore, provideMockStore } from "@ngrx/store/testing";
 import { AppComponent } from "./app.component";
 import { HeaderModule } from "./features/header/header.module";
+import { PhotosState } from "./store/photos/photos.state";
 
 describe('AppComponent', () => {
   const initialState = {};
+  let store: MockStore<PhotosState>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,10 +18,14 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        provideMockStore({ initialState })
+      ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
+    store = TestBed.inject(MockStore);
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,18 @@
 import { Component, ChangeDetectionStrategy, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { loadFavorites } from "./store/favorites/favorites.actions";
+import { FavoritesState } from "./store/favorites/favorites.state";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  constructor(private store: Store<FavoritesState>) {}
+  ngOnInit(): void {
+    this.store.dispatch(loadFavorites());
+  }
+}
