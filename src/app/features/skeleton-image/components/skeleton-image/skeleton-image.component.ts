278fb4a2393f7cd9efa7ce31119d432992268411
Anton Naidunou <Anton_Naidunou@epam.com>
2023-03-01 20:07:53 +0100

    Optimize image load event listeners

--- src/app/features/skeleton-image/components/skeleton-image/skeleton-image.component.ts
+++ src/app/features/skeleton-image/components/skeleton-image/skeleton-image.component.ts
@@ -1,21 +1,41 @@
-import { ChangeDetectionStrategy, Component, Input, Output, EventEmitter } from '@angular/core';
-
+import { OnInit, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, Output, EventEmitter, ElementRef, Renderer2 } from '@angular/core';
+import { fromEvent, Subject, take, takeUntil } from 'rxjs';
 @Component({
   selector: 'app-skeleton-image',
   templateUrl: './skeleton-image.component.html',
   styleUrls: ['./skeleton-image.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SkeletonImageComponent {
+export class SkeletonImageComponent implements AfterViewInit {
   @Input() src!: string;
   @Input() alt!: string;
   @Input() isScaled = false;
   @Output() onImageLoaded = new EventEmitter<void>();
 
-  isLoaded = false;
+  @ViewChild('loadedImage') image?: ElementRef;
 
-  onImageLoad(): void {
+  isLoaded = false;
+  private destroyed$ = new Subject<void>();
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    fromEvent(this.image?.nativeElement, 'load', { once: true }) // fromEvent will remove events after fires once
+      .pipe(
+        take(1),
+        takeUntil(this.destroyed$)
+      ).subscribe(() => this.onImageLoad());
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+
+  onImageLoad() {
     this.isLoaded = true;
     this.onImageLoaded.emit();
+    this.cdr.detectChanges();
   }
 }
